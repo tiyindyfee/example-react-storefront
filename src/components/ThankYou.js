ba342de92a92edf40467a05923f06e67fc4d5032
Collin Schneider <collin@theironyard.com>
2017-03-29 15:13:52 -0400

    Fixed thank you

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class ThankYou extends React.Component {
     render() {
-        return <div class="has-text-centered">
+        return <div className="has-text-centered">
             <img src="/img/thankyou.gif" alt="Thank You" />
         </div>
     }
